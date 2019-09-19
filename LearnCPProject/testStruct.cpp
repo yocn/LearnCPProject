b76b9326d5e6736932fd8e1c3b2bf4f46ae1d2c5
Yocn <zhaoyingkun@163.com>
2019-09-19 11:48:38 +0800

    结构体

--- LearnCPProject/testStruct.cpp
+++ LearnCPProject/testStruct.cpp
@@ -7,7 +7,11 @@
 
 void test() {
     __t_struct1 t1;
+    __t_struct1 *t1p = new __t_struct1;
     __t_struct2 t2 = new __t_struct1;
+    __t_struct2 t3 = t1p;
     printf("%ld,%s\n", t1.id, t1.str);
-    printf("%ld,%s", t2->id, t2->str);
+    printf("%ld,%s\n", t1p->id, t1p->str);
+    printf("%ld,%s\n", t2->id, t2->str);
+    printf("%ld,%s\n", t3->id, t3->str);
 }
--- LearnCPProject/testStruct.h
+++ LearnCPProject/testStruct.h
@@ -9,8 +9,9 @@ struct t_struct {
     long id = 10;
     char *str = const_cast<char *>("t_struct");
 };
-//两种定义方式区别
+//两种定义方式区别t_struct def为__t_struct1
 typedef struct t_struct __t_struct1;
+//两种定义方式区别t_struct def为一个指针，__t_struct2作为一个指针使用
 typedef struct t_struct *__t_struct2;
 
 void test();
